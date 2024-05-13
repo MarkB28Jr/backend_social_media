05b45ce051c65d92413c3eb00aec6d6a9d749563
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-13 14:42:50 -0500

    Schemas made

--- models/Social.js
+++ models/Social.js
@@ -1,10 +1,49 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
  
+
+const MessagingSchema = new Schema({
+    title: String,
+    name: String,
+    username: String,
+    content: String,
+    // date: Date,
+},
+{ timestamps: true }
+)
+
+const CommunitySchema = new Schema ({
+    communityName: String,
+    email: String,
+    username: String,
+    content: String,
+},
+{ timestamps: true })
+
+const ProfileSchema = new Schema ({
+    profileName: String,
+    email: String,
+    username: String,
+    age: String,
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'other']
+    },
+    image:[{
+        type: String
+    }],
+},
+{ timestamps: true })
+
 const SocialSchema = new Schema({
     name: String,
     email: String,
     birthday: String,
-}, { timestamps: true });
+    username: String,
+    message: {MessagingSchema},
+    community: {CommunitySchema},
+    profile: {ProfileSchema},
+},
+{ timestamps: true });
 
 module.exports = mongoose.model("Social", SocialSchema)
