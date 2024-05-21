a5afb74683525b2f940822cde0905c65f2c732c5
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-21 13:54:04 -0500

    updating cors to allow frontend to interact

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const usersRouter = require('../controllers/users')
-// const {authenticateUser} = require('../middlewares/auth')
 
 /*************** Register User ***************/
 router.post('/register', usersRouter.registerUser)
